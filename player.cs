c5ebfed4e076e53cb6f63e7045c5c27aaedef304
User <User>
2024-02-16 01:07:52 -0500

    Initial project + Basic player controls

---
+++ player.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+using static Godot.GD;
+
+
+public partial class player : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+	public override void _Ready()
+	{
+		// Called every time the node is added to the scene.
+		// Initialization here.
+		GD.Print("Hello from C# to Godot :)");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor()) {
+			velocity.Y -= gravity * (float)delta;
+			GD.Print("Not Is Onfloor");
+		} else {
+			GD.Print("Is Onfloor");
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
